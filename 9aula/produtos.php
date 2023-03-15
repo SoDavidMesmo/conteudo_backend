b5b718b3fa1dcb036806c0e253b4c6bbe339df8e
SoDavidMesmo <david.santos@alunos.ifsuldeminas.edu.br>
2023-03-15 11:28:25 -0300

    9aula

---
+++ 9aula/produtos.php
@@ -0,0 +1,25 @@
+<?php
+
+$produto1= [
+            "nome" => "Câmera",
+            "marca" => "LG",
+            "preco" => "2500",
+];
+
+$produto2= [
+    "nome" => "Notebook",
+    "marca" => "dell",
+    "preco" => "7500",
+];
+
+$produto3= [
+    "nome" => "Smartphone",
+    "marca" => "Samsung",
+    "preco" => "3500",
+];
+
+$produtos = [$produto1,$produto2,$produto3];
+
+
+
+    ?>
