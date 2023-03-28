47ddced8c5e39b9cd8c150090fbfafa020c71b92
SoDavidMesmo <david.santos@alunos.ifsuldeminas.edu.br>
2023-03-28 08:48:29 -0300

    11aula

--- 11aula/exemplo_post/login.php
+++ 11aula/exemplo_post/login.php
@@ -1,15 +1,19 @@
 <?php 
+//se existe, ou foi enviada, a variável usuário
    if (isset($_POST['Usuario'])){
-   if($_POST['Usuario'] == "David" && $_POST['Senha'] == 12345){
-      echo "Logado";
-   } else{
-      echo "Erro";
+      //verifica se o usuario é igual a david e a senha igual a 12345
+      if($_POST['Usuario'] == "David" && $_POST['Senha'] == 12345){
+         echo "Logado";
+      }else{
+         echo "Erro";
+      }
    }else{
       //manda abrir outro arquivo, o formulário de login
-      //caso o usuario
+      //caso o usuario tente abrir esse arquivo pela URL
+      //sem acessar o formulário de login "redirecionamento
       header("Location:form_login.html");
    }
-}
+
 
 ?>
 
