f831a94a77aa75157cd7a35df7165edea9664a6d
David Santos Silva <125364549+SoDavidMesmo@users.noreply.github.com>
2023-03-29 09:20:38 -0300

    O que são Variáveis/Operadores

---
+++ 2aula/variaveis.php
@@ -0,0 +1,26 @@
+'<?php
+/*
+     Variável é um espaço de memória utilizado para armazenar valores que serão utilizados durante a execução do programa.
+
+     Para criar uma variável em PHP utilizamos o $ na frente do nome da variável. 
+     A tipagem no PHP é dinâmica e ela é criada quando fazemos uma atribuição.
+
+*/
+
+$nome_variavel=10;
+$outra_var="José";
+$mais_uma=5.5;
+
+//exemplo de como imprimir o conteudo de uma
+//variavel
+echo $nome_variavel;
+
+/*
+Regras para criar o nome das variáveis:
+1) não pode começar com números
+2) não pode ter espaços
+3) alguns caracteres especiais não são permitidos
+*/
+
+
+?>
