a6313739d5a99c7e9863c4c6a1e25facbbccf727
David Santos Silva <125364549+SoDavidMesmo@users.noreply.github.com>
2023-04-25 08:43:53 -0300

    Ligando Bdds com PHP

---
+++ aula13/usuario/inserir.php
@@ -0,0 +1,16 @@
+<?php
+    //importa o arquivo de conexão
+    require_once"../conexao.php";
+
+    //cria uma variável com um comando SQL
+    $SQL="INSERT INTO `usuario` (`nome`, `login`, `senha`) VALUES ('Ivan Paulino', 'admin123', '123');";
+
+    //prepara o comando para ser executado no mysql
+    $comando=$conexao->prepare($SQL);
+
+    //executar o comando
+    $comando->execute();
+
+
+
+?>
